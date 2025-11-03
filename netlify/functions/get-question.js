e5c8655cdd940dda10025b4f36bb8ea7b882b2e9
Dukimonster <60096396+Dukimonster@users.noreply.github.com>
2025-11-03 21:29:24 +0100

    Update get-question.js

--- netlify/functions/get-question.js
+++ netlify/functions/get-question.js
@@ -1,28 +1,15 @@
-import { getStore } from '@netlify/blobs';
+exports.handler = async () => {
+  const questions = [
+    { id: 1, A: "Aldrig kunne grine igen", B: "Aldrig kunne sove igen", votesA: 0, votesB: 0 },
+    { id: 2, A: "Kunne tale med dyr", B: "Kunne flyve", votesA: 0, votesB: 0 },
+    { id: 3, A: "Spise pizza hver dag", B: "Aldrig spise pizza igen", votesA: 0, votesB: 0 }
+  ];
 
-export const handler = async () => {
-  const store = getStore({ name: 'wyr-store' });
-  let json = await store.get('questions', { type: 'json' });
-
-  if (!json) {
-    json = {
-      questions: [
-        { id: 1,  A: "Komme til at tisse lidt hver gang du griner", B: "Komme til at slå en lydløs, men meget lugtende prut hver gang du rejser dig", votesA: 0, votesB: 0 },
-        { id: 2,  A: "Komme til at sige “tak” hver gang nogen fornærmer dig", B: "Komme til at fnise hver gang nogen græder", votesA: 0, votesB: 0 },
-        { id: 3,  A: "Have lidt toiletpapir hængende fra bukserne én gang om ugen", B: "Få en plet sved i armhulen, hver gang du skal tage et billede", votesA: 0, votesB: 0 },
-        { id: 4,  A: "Skulle bruge et offentligt toilet uden lås", B: "Skulle tørre dig med vådt toiletpapir hver gang", votesA: 0, votesB: 0 },
-        { id: 5,  A: "Komme til at prutte diskret i en elevator, og alle tror det er dig", B: "Komme til at hoste en lille snotklat ud, hver gang du griner", votesA: 0, votesB: 0 }
-      ]
-    };
-    await store.set('questions', JSON.stringify(json));
-  }
-
-  const arr = json.questions;
-  const q = arr[Math.floor(Math.random() * arr.length)];
+  const random = questions[Math.floor(Math.random() * questions.length)];
 
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(q)
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(random)
   };
 };
